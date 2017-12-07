9150275b9dfe8c8a19149ea300fefd0330c09d17
jaegyu <jpmlyr@gmail.com>
2017-12-07 16:37:02 +0900

    개인정보 기능 구현

--- src/main/java/me/jaegyu/domain/User.java
+++ src/main/java/me/jaegyu/domain/User.java
@@ -1,6 +1,18 @@
-package me.jaegyu.web;
+package me.jaegyu.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(nullable = false, length=20)
 	private String userId;
 	private String password;
 	private String name;
@@ -37,10 +49,18 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public void update(User newUser) {
+		this.password = newUser.password;  //?
+		this.name = newUser.name;
+		this.email = newUser.email;
+		
+	}
 
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
 	}
+
 
 }
