ee35c475bb92fb5a2b345364593d6e86784cee8e
jaegyu <jpmlyr@gmail.com>
2017-12-12 10:03:34 +0900

    로그인 메뉴 관리 및 로그아웃 구현

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO USER (EMAIL, NAME, PASSWORD, USER_ID) VALUES ('alice@hotmail.com', 'alice', '1234', 'alice'); 
+INSERT INTO USER (EMAIL, NAME, PASSWORD, USER_ID) VALUES ('bob@hotmail.com', 'bob', '1234', 'bob'); 
