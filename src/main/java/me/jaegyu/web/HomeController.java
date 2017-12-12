07aa9fcafac99c4e5aa1e4dbe61923b4d1448f38
jaegyu <jpmlyr@gmail.com>
2017-12-12 17:16:45 +0900

    외부 서버 tomcat에 war 배포하도록 수정

--- src/main/java/me/jaegyu/web/HomeController.java
+++ src/main/java/me/jaegyu/web/HomeController.java
@@ -1,13 +1,21 @@
 package me.jaegyu.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import me.jaegyu.domain.QuestionRepository;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private QuestionRepository repository;
 
 	@RequestMapping("/")
-	public String home(){
+	public String home(Model model){
+		model.addAttribute("questions", repository.findAll());
 		return "index";
 	}
 }
