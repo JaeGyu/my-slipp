3052602338777370d0a1bcc4860d0c816d72eb5c
jaegyu <jpmlyr@gmail.com>
2017-12-12 16:01:42 +0900

    질문하기 기능 구현

---
+++ src/main/java/me/jaegyu/web/QuestionController.java
@@ -0,0 +1,44 @@
+package me.jaegyu.web;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import me.jaegyu.domain.Question;
+import me.jaegyu.domain.QuestionRepository;
+import me.jaegyu.domain.User;
+
+@Controller
+@RequestMapping("/questions")
+public class QuestionController {
+	
+	@Autowired
+	private QuestionRepository repository;
+	
+	@GetMapping("/form")
+	public String form(HttpSession session){
+		if(!HttpSessionUtils.isLoginUser(session)){
+			return "redirect:/users/loginForm";
+		}
+		return "/qna/form";
+	}
+	
+	@PostMapping("")
+	public String create(String title, String contents, HttpSession session){
+		if(!HttpSessionUtils.isLoginUser(session)){
+			return "redirect:/users/loginForm";
+		}
+		
+		User sessionedUser = HttpSessionUtils.getUserFromSession(session);
+		Question question = new Question(sessionedUser.getUserId(), title, contents);
+		
+		repository.save(question);
+		
+		return "redirect:/";
+	}
+
+}
