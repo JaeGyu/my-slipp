483a135ee4fd93ad4fc169d1692a5e6c22b9ac75
jaegyu <jpmlyr@gmail.com>
2017-12-18 11:22:11 +0900

    질문 수정 삭제 기능 구현 완료

--- src/main/java/me/jaegyu/web/QuestionController.java
+++ src/main/java/me/jaegyu/web/QuestionController.java
@@ -5,9 +5,11 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import me.jaegyu.domain.Question;
@@ -17,42 +19,56 @@ import me.jaegyu.domain.User;
 @Controller
 @RequestMapping("/questions")
 public class QuestionController {
-	
+
 	@Autowired
 	private QuestionRepository repository;
-	
+
 	@GetMapping("/form")
-	public String form(HttpSession session){
-		if(!HttpSessionUtils.isLoginUser(session)){
+	public String form(HttpSession session) {
+		if (!HttpSessionUtils.isLoginUser(session)) {
 			return "redirect:/users/loginForm";
 		}
 		return "/qna/form";
 	}
-	
+
 	@PostMapping("")
-	public String create(String title, String contents, HttpSession session){
-		if(!HttpSessionUtils.isLoginUser(session)){
+	public String create(String title, String contents, HttpSession session) {
+		if (!HttpSessionUtils.isLoginUser(session)) {
 			return "redirect:/users/loginForm";
 		}
-		
+
 		User sessionedUser = HttpSessionUtils.getUserFromSession(session);
 		Question question = new Question(sessionedUser, title, contents);
-		
+
 		repository.save(question);
-		
+
 		return "redirect:/";
 	}
-	
+
 	@GetMapping("/{id}")
-	public String show(@PathVariable("id") Long id, Model model){
+	public String show(@PathVariable("id") Long id, Model model) {
 		model.addAttribute("question", repository.findOne(id));
 		return "/qna/show";
 	}
-	
+
 	@GetMapping("/{id}/form")
-	public String updateForm(@PathVariable Long id, Model model){
+	public String updateForm(@PathVariable Long id, Model model) {
 		model.addAttribute("question", repository.findOne(id));
 		return "/qna/updateForm";
+	}
+
+	@PutMapping("/{id}")
+	public String update(@PathVariable("id") Long id, String title, String contents) {
+		Question question = repository.findOne(id);
+		question.update(title, contents);
+		repository.save(question);
+		return String.format("redirect:/questions/%d", id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable("id") Long id){
+		repository.delete(id);
+		return "redirect:/";
 	}
 
 }
